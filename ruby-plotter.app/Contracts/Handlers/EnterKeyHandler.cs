2c0aa6cd5d6b0b2e932ec9f9091a18993a42ce65
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2025-08-17 16:34:58 +0200

    Added keys behavior handlers

---
+++ ruby-plotter.app/Contracts/Handlers/EnterKeyHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ruby_plotter.app.Contracts.Handlers;
+
+public static class EnterKeyHandler
+{
+    public static readonly DependencyProperty IsEnabledProperty =
+        DependencyProperty.RegisterAttached(
+            "IsEnabled",
+            typeof(bool),
+            typeof(EnterKeyHandler),
+            new PropertyMetadata(false, OnIsEnabledChanged));
+
+    public static bool GetIsEnabled(DependencyObject obj) => (bool)obj.GetValue(IsEnabledProperty);
+    public static void SetIsEnabled(DependencyObject obj, bool value) => obj.SetValue(IsEnabledProperty, value);
+
+    private static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is UIElement element)
+        {
+            if ((bool)e.NewValue)
+            {
+                element.KeyDown += OnKeyDown;
+            }
+            else
+            {
+                element.KeyDown -= OnKeyDown;
+            }
+        }
+    }
+
+    private static void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var request = new TraversalRequest(FocusNavigationDirection.Next);
+
+            (Keyboard.FocusedElement as UIElement)?.MoveFocus(request);
+            e.Handled = true;
+        }
+    }
+}
