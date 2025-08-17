1e123405307e9b0e79b39d90ed54acba185ce554
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2025-08-17 15:03:53 +0200

    update pages

---
+++ ruby-plotter.app/Services/DoubleValidationRuleService.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace ruby_plotter.app.Services;
+
+public class DoubleValidationRuleService : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        // Attempt to interpret the input value as a double.
+        if (double.TryParse(value as string, out double result))
+        {
+            return ValidationResult.ValidResult;
+        }
+        // Return an error if the value does not constitute a valid double.
+        return new ValidationResult(false, "The number format is invalid. Kindly input a valid decimal number.");
+    }
+}
