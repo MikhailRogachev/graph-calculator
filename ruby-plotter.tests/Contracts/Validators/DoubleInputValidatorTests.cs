6985b167133f99210ecc8155ace8648f3d7cdee0
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2025-09-04 15:11:07 +0200

    update validation system

---
+++ ruby-plotter.tests/Contracts/Validators/DoubleInputValidatorTests.cs
@@ -0,0 +1,41 @@
+﻿using ruby_plotter.app.Contracts.Validators;
+using System.Globalization;
+
+namespace ruby_plotter.tests.Contracts.Validators;
+
+public class DoubleInputValidatorTests
+{
+
+    [Theory, MemberData(nameof(GetDataToValidate))]
+    public void ValidDoubleInputTest(string input, bool isPositiveOnly, bool expectedValue)
+    {
+        // Arrange
+        var validator = new DoubleInputValidator
+        {
+            IsPositiveOnly = isPositiveOnly
+        };
+
+        // Act
+        var result = validator.Validate(input, CultureInfo.CurrentCulture);
+        // Assert
+        Assert.True(result.IsValid == expectedValue);
+    }
+
+    public static TheoryData<string, bool, bool> GetDataToValidate()
+    {
+        return new TheoryData<string, bool, bool>
+        {
+            { "123.45", true, true  },
+            { string.Empty, true, false },
+            { "", true, false  },
+            { " ", true, false  },
+            { "123,45", true, false  },
+            { "1h3,45", true, false  },
+            { "-123.45", true, false  },
+            { "-123.45", false, true  },
+            { "-12", false, true  },
+            { "-12", true, false  },
+            { "1", true, true  },
+        };
+    }
+}
