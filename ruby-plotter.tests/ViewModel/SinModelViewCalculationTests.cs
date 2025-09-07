77700fd79f85c60e33c1ab670933405e0e2ebd7d
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2025-09-07 21:54:21 +0200

    Added new panel

---
+++ ruby-plotter.tests/ViewModel/SinModelViewCalculationTests.cs
@@ -0,0 +1,36 @@
+﻿using ruby_plotter.app.Contracts.Options;
+using ruby_plotter.app.Contracts.Parameters;
+using ruby_plotter.app.ViewModel;
+using ruby_plotter.tests.Utilites.Helpers;
+using UnitTests.Utility.Attributes;
+
+namespace ruby_plotter.tests.ViewModel;
+
+public class SinModelViewCalculationTests
+{
+    public static SinCosParameter GetParameter() => TestHelper.GetSinCosParameter();
+    public static TrigonometricValidationSettings GetSettings()
+        => TestHelper.GetTrigonometricValidationSettings();
+
+
+    [Theory, AutoMock]
+    public void GradToRadCalculationTest(
+        [RegInstance(nameof(GetParameter))] SinCosParameter parameter,
+        [RegInstance(nameof(GetSettings))] TrigonometricValidationSettings settings
+        )
+    {
+        // Arrange
+        var value = 90;
+        var valueRad = 90 * Math.PI / 180;
+        var model = new SinViewModel(parameter, settings);
+        model.Phase = value;
+
+        // Act
+        var radMeasure = model.PhaseMeasures.First(p => p.Id == 2);
+        model.SelectedPhaseMeasure = radMeasure;
+
+        // Assert
+        Assert.Equal(valueRad, model.Phase);
+    }
+
+}
